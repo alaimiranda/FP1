069845a44b248c79188d6f8c4eed4c118c04885d
Alai Miranda Blanco Lascurain <alaimbl@gmail.com>
2022-09-13 19:01:58 +0200

    Creando el repositorio con el primer programa

---
+++ tema2/tema2_1_1.cpp
@@ -0,0 +1,8 @@
+//Este programa halla el volumen de un como de 26.79 de altura y radio de la base 14.5
+#include <iostream>
+using namespace std;
+
+int main () {
+  cout << "El volumen es " << (3.141592*14.5*14.5*26.79)/3 << endl;
+  return 0;
+}
